7a9a7308d714a600c231d85a2ae8f7bfc9b559fd
Jyotish P <me@jyotishp.ml>
2021-11-18 05:11:24 +0530

    Add sleep to explore pod

--- test_random.py
+++ test_random.py
@@ -1,10 +1,15 @@
 import json
+import os
+import time
 import numpy as np
 from evaluator.base_predictor import AIcrowdPredictor
 
 
 class RandomPredictor(AIcrowdPredictor):
     def __init__(self):
+        if not os.path.exists("/tmp/let-go"):
+            time.sleep(10)
+
         self.model = None
         self.model_path = "models/model.json"
         self.load_model()
